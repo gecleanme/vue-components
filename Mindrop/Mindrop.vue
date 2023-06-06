b4c084a013e5b01dd71a99d67d54c16832686347
gecleanme <130738860+gecleanme@users.noreply.github.com>
2023-06-06 23:18:06 +0300

    Added dynamic title and icon props

--- Mindrop/Mindrop.vue
+++ Mindrop/Mindrop.vue
@@ -2,22 +2,24 @@
 
 import {ref, watch} from "vue";
 
-const showItems= ref(false)
+const showItems = ref(false)
 
-const props= defineProps({
+const props = defineProps({
     containerClasses: String,
     triggerClasses: String,
-    listWrapperClasses: String
+    listWrapperClasses: String,
+    buttonName: String,
+    buttonIcon: String
 })
 
-const closeFromOutside = (event) =>{
-    if(!event.target.closest('.mindrop')){
+const closeFromOutside = (event) => {
+    if (!event.target.closest('.mindrop')) {
         showItems.value = false;
     }
 }
 
-watch(showItems, () =>{
-    if (showItems.value){
+watch(showItems, () => {
+    if (showItems.value) {
         document.addEventListener('click', closeFromOutside)
     }
 })
@@ -26,13 +28,17 @@ watch(showItems, () =>{
 <template>
     <div class="mindrop relative" :class="props.containerClasses">
 
-    <div class="bg-black rounded px-2 py-3 text-white" @click="showItems=!showItems" :class="props.triggerClasses" >
-        <slot name="trigger"></slot>
-    </div>
+        <div class="bg-black rounded px-2 py-3 text-white flex space-x-2">
+            <i class="material-icons">{{ buttonIcon }}</i>
+            <button @click="showItems=!showItems" :class="props.triggerClasses">{{ props.buttonName }}
+            </button>
+        </div>
 
-    <div  class="bg-black absolute mt-2 px-3 py-2 rounded-md shadow-lg text-white z-50" v-show="showItems" :class="props.listWrapperClasses">
-        <slot/>
-      </div>
+        <div class="bg-black absolute mt-2 px-3 py-2 rounded-md shadow-lg text-white z-50" v-show="showItems"
+             :class="props.listWrapperClasses">
+            <slot/>
+        </div>
+
     </div>
 </template>
 

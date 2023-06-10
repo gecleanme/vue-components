a0f8d3ec43c3a5184f763f9a0eec10faea5200be
gecleanme <130738860+gecleanme@users.noreply.github.com>
2023-06-10 19:51:57 +0300

    Added Blinkr

---
+++ Blinkr/Blinkr.vue
@@ -0,0 +1,73 @@
+<template>
+    <transition name="fade">
+        <div v-if="visible" class="flash-message" :class="type">
+            {{ message }}
+        </div>
+    </transition>
+</template>
+
+<script setup>
+import {ref, watchEffect} from 'vue'
+import {usePage} from "@inertiajs/vue3";
+
+const props = defineProps({
+    message: String,
+    type: {
+        type: String,
+        default: 'info'
+    },
+    duration: {
+        type: Number,
+        default: 3000
+    }
+})
+
+const visible = ref(false)
+
+watchEffect(() => {
+    if (usePage().props.flash.success) {
+        visible.value = true
+        setTimeout(() => {
+            visible.value = false
+        }, props.duration)
+    }
+})
+</script>
+
+<style scoped>
+.flash-message {
+    @apply p-4 rounded mb-4 text-center fixed top-4 right-4 z-50;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    @apply transition-opacity duration-500;
+}
+
+.fade-enter,
+.fade-leave-to {
+    @apply opacity-0;
+}
+
+.flash-message {
+    @apply rounded-lg p-4;
+}
+
+.flash-message.info {
+    @apply bg-blue-100 text-blue-700;
+}
+
+.flash-message.success {
+    @apply bg-green-100 text-green-700;
+}
+
+.flash-message.warning {
+    @apply bg-yellow-100 text-yellow-700;
+}
+
+.flash-message.error {
+    @apply bg-red-100 text-red-700;
+}
+
+
+</style>
